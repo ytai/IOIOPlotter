abe60fd6842cfef10eca29bd3871ca0c6af90dd0
Ytai Ben-Tsvi <ytaibt@gmail.com>
2013-05-13 02:17:22 -0700

    Custom icon

--- src/mobi/ioio/plotter_app/PlotterMainActivity.java
+++ src/mobi/ioio/plotter_app/PlotterMainActivity.java
@@ -17,7 +17,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.MutableContextWrapper;
 import android.content.ServiceConnection;
 import android.net.Uri;
 import android.os.Bundle;
